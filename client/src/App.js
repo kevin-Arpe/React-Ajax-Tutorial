5f27941c017f535697ec1b3436621b1ce2681581
kevinLee <arpeggiogio@gmail.com>
2020-03-08 19:47:25 +0900

    React Ajax Basic Setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; Ajax
+      </article>
     </div>
   );
 }
