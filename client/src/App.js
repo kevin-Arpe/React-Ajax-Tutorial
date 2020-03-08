60ddc2785cb496e79e0892e128d16f6cb19ce2e8
kevinLee <arpeggiogio@gmail.com>
2020-03-08 20:33:11 +0900

    Get Lists Using Fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import './App.css';
 
+class Nav extends React.Component {
+  state = {
+    list: []
+  };
+
+  componentDidMount() {
+    fetch('list.json')
+      .then(function(result) {
+        return result.json();
+      })
+      .then(function(json) {
+        console.log(json);
+        this.setState({list: json});
+      }.bind(this))
+  }
+
+  render() {
+    let listTag = [];
+    for(let i=0; i<this.state.list.length; i++) {
+      let li = this.state.list[i];
+      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
+    }
+    return (
+      <nav>
+        <ul>
+          {listTag}
+          <h2>Hello</h2>
+        </ul>
+      </nav>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
       <h1>WEB</h1>
-      <nav>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-        </ul>
-      </nav>
+      <Nav />
       <article>
         <h2>Welcome</h2>
         Hello, React &amp; Ajax
